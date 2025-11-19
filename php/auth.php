fad4ef1f2a197347dd668ba10a2f372cb8d6800e
jubit25 <bitju05@gmail.com>
2025-11-19 21:12:27 +0800

    auth.php

--- php/auth.php
+++ php/auth.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Check if the user is logged in, if not then redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
